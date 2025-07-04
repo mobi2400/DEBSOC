274e4a1f525eab2eb311a07790f897a63a42d0a2
mobasshir <mobasshirkhan9931@gmail.com>
2025-07-04 22:45:03 +0530

    DSA

--- hello.cpp
+++ hello.cpp
@@ -4,5 +4,7 @@ using namespace std;
 int main()
 {
     cout << "Harsh ki makichut badi majboot daale ganna nikle juice ";
+    int static A;
+    // A is a global variable use only 4 byte of pace in stack memory
     return 0;
 }
